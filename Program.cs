14f49425726480ad5e203799c81071343ab2903b
marekkotowski <marek-kotowski@o2.pl>
2021-10-06 21:51:13 +0200

    zamiana struktury

--- Program.cs
+++ Program.cs
@@ -13,15 +13,17 @@ namespace WzorceProjektowe_1
         {
             BaseCreator mssqlkreator = new BaseMSSQLCreator();
             IBase SoruceBaza =  mssqlkreator.CreateBase();
-            SoruceBaza.ConnectionString =  mssqlkreator.CreateConnectiontring("zapr-test", "zapr-test");
+            SoruceBaza.ConnectionString =  mssqlkreator.CreateConnectiontring("10.0.0.1", "mssqlbase");
 
+            Console.WriteLine(SoruceBaza.ConnectionString);
             SoruceBaza.Connect();
             SoruceBaza.SQLInsert();
 
             BaseCreator odbckreator = new BaseODBCCreator();
             IBase TargetBaza = odbckreator.CreateBase();
             
-            TargetBaza.ConnectionString = odbckreator.CreateConnectiontring("zapr-kooperacja", "zapr-kooperacja");
+            TargetBaza.ConnectionString = odbckreator.CreateConnectiontring("odbcbase", "testdatabase");
+            Console.WriteLine(TargetBaza.ConnectionString);
             TargetBaza.Connect();
             TargetBaza.SQLInsert(); 
       
