2304af650007bea48cde96c7b7a8b07e354547f1
marekkotowski <marek-kotowski@o2.pl>
2021-10-06 21:55:43 +0200

    poprawki

--- Program.cs
+++ Program.cs
@@ -11,24 +11,36 @@ namespace WzorceProjektowe_1
     {
         static void Main(string[] args)
         {
-            BaseCreator mssqlkreator = new BaseMSSQLCreator();
-            IBase SoruceBaza =  mssqlkreator.CreateBase();
-            SoruceBaza.ConnectionString =  mssqlkreator.CreateConnectiontring("10.0.0.1", "mssqlbase");
-
-            Console.WriteLine(SoruceBaza.ConnectionString);
-            SoruceBaza.Connect();
-            SoruceBaza.SQLInsert();
-
-            BaseCreator odbckreator = new BaseODBCCreator();
-            IBase TargetBaza = odbckreator.CreateBase();
-            
-            TargetBaza.ConnectionString = odbckreator.CreateConnectiontring("odbcbase", "testdatabase");
-            Console.WriteLine(TargetBaza.ConnectionString);
-            TargetBaza.Connect();
-            TargetBaza.SQLInsert(); 
+            FactoryMethod(); 
       
 
             Console.ReadLine(); 
+        }
+
+
+        static void FactoryMethod()
+        {
+            ICollection<IBase> Bazy = new List<IBase>();
+
+
+            BaseCreator mssqlkreator = new BaseMSSQLCreator();
+            IBase SoruceBaza = mssqlkreator.CreateBase();
+            SoruceBaza.ConnectionString = mssqlkreator.CreateConnectiontring("10.0.0.1", "mssqlbase");
+            Console.WriteLine(SoruceBaza.ConnectionString);
+            Bazy.Add(SoruceBaza);
+
+            BaseCreator odbckreator = new BaseODBCCreator();
+            IBase TargetBaza = odbckreator.CreateBase();
+            TargetBaza.ConnectionString = odbckreator.CreateConnectiontring("odbcbase", "testdatabase");
+            Console.WriteLine(TargetBaza.ConnectionString);
+            Bazy.Add(TargetBaza);
+
+            foreach (IBase baza in Bazy)
+            {
+                baza.Connect();
+                baza.SQLInsert(); 
+            }
+
         }
     }
 }
